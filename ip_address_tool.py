1da31ee351cbd86311cf7e77fe749c931d0e7303
uS-aito <h2so40627@gmail.com>
2017-04-04 14:06:10 +0900

    /で区切られたアドレスに対応した動作モードを追加

--- ip_address_tool.py
+++ ip_address_tool.py
@@ -17,48 +17,59 @@ class IpAddressTool(object):
     
     # print network, minimam host, maximam host, broadcast ip address
     def print_special_ip(self,ip_address):
+        def long_address_mode(ip_address):
+            pass
+
+        def slash_mode(ip_address):
+            # split ip_address
+            (address, subnet_mask) = ip_address.split("/")
+            address = address.split(".")
+
+            # make network address
+            target = (int(subnet_mask)-1)//8
+            bitmask = 255 << (8-int(subnet_mask))%8
+            bitmask = bitmask & 255
+            address[target] = str(int(address[target]) & bitmask)
+            for i in range(target+1,4):
+                address[i] = "0"
+            network_address = ".".join(address)
+            network_address = network_address + "/" + subnet_mask
+
+            # make youngest host address
+            address[3] = str(int(address[3])+1)
+            youngest_host_address = ".".join(address)
+            youngest_host_address = youngest_host_address + "/" + subnet_mask
+
+            # make broadcast address
+            address[3] = str(int(address[3])-1)
+            bitmask = 255 - bitmask
+            address[target] = str(int(address[target]) | bitmask)
+            for i in range(target+1,4):
+                address[i] = "255"
+            broadcast_address = ".".join(address)
+            broadcast_address = broadcast_address + "/" + subnet_mask
+
+            # make oldest host address
+            address[3] = str(int(address[3])-1)
+            oldest_host_address = ".".join(address)
+            oldest_host_address = oldest_host_address + "/" + subnet_mask
+
+            # print special addresses
+            print("Network address: %s" % network_address)
+            print("Youngest host address: %s" % youngest_host_address)
+            print("Oldest host address: %s" % oldest_host_address)
+            print("Broadcast address: %s" % broadcast_address)
+
         # check ip_address is valid
         if not self.is_valid_ip(ip_address):
             raise ValueError()
 
-        # split ip_address
-        (address, subnet_mask) = ip_address.split("/")
-        address = address.split(".")
+        if "/" in ip_address:
+            slash_mode(ip_address)
+        else:
+            pass
 
-        # make network address
-        target = (int(subnet_mask)-1)//8
-        bitmask = 255 << (8-int(subnet_mask))%8
-        bitmask = bitmask & 255
-        address[target] = str(int(address[target]) & bitmask)
-        for i in range(target+1,4):
-            address[i] = "0"
-        network_address = ".".join(address)
-        network_address = network_address + "/" + subnet_mask
 
-        # make youngest host address
-        address[3] = str(int(address[3])+1)
-        youngest_host_address = ".".join(address)
-        youngest_host_address = youngest_host_address + "/" + subnet_mask
-
-        # make broadcast address
-        address[3] = str(int(address[3])-1)
-        bitmask = 255 - bitmask
-        address[target] = str(int(address[target]) | bitmask)
-        for i in range(target+1,4):
-            address[i] = "255"
-        broadcast_address = ".".join(address)
-        broadcast_address = broadcast_address + "/" + subnet_mask
-
-        # make oldest host address
-        address[3] = str(int(address[3])-1)
-        oldest_host_address = ".".join(address)
-        oldest_host_address = oldest_host_address + "/" + subnet_mask
-
-        # print special addresses
-        print("Network address: %s" % network_address)
-        print("Youngest host address: %s" % youngest_host_address)
-        print("Oldest host address: %s" % oldest_host_address)
-        print("Broadcast address: %s" % broadcast_address)
 
 def main():
     iat = IpAddressTool()
